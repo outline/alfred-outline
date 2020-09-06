5cf65028449da31ff7f43273446926344ec6f273
Ravnoor Gill <ravnoor@gmail.com>
2020-09-05 20:26:32 -0400

    added support for self-hosted instances

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const alfy = require('alfy');
 
-const results = await alfy.fetch(`https://www.getoutline.com/api/documents.search`, {
+const results = await alfy.fetch(`${process.env.subdomain || "www.getoutline.com"}/api/documents.search`, {
   method: "POST",
   headers: {
     Authorization: `Bearer ${process.env.apiToken}`,
@@ -15,7 +15,8 @@ const results = await alfy.fetch(`https://www.getoutline.com/api/documents.searc
 let items = results.data.map(result => ({
   title: result.document.title,
   subtitle: result.context.replace(/\<\/?b\>/g, ''),
-  arg: `https://${process.env.subdomain || "www"}.getoutline.com${result.document.url}`,
+  // arg: `https://${process.env.subdomain || "www"}.getoutline.com${result.document.url}`,
+  arg: `https://${process.env.subdomain || "www.getoutline.com"}${result.document.url}`,
   icon: {
     path: path.join(__dirname, "document.png")
   }
@@ -24,7 +25,8 @@ let items = results.data.map(result => ({
 if (!items.length) {
   items = [{
     title: "No results - go to Outline homepage",
-    arg: `https://${process.env.subdomain || "www"}.getoutline.com/home`
+    // arg: `https://${process.env.subdomain || "www"}.getoutline.com/home`
+    arg: `https://${process.env.subdomain || "www.getoutline.com"}`
   }];
 }
 
