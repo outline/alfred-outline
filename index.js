7265479d397c80055457bb7f01e310a5acdf116d
Tom Moor <tom.moor@gmail.com>
2020-09-11 12:30:36 -0700

    www -> app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const alfy = require('alfy');
 
-const results = await alfy.fetch(`${process.env.domain || "www.getoutline.com"}/api/documents.search`, {
+const results = await alfy.fetch(`${process.env.domain || "app.getoutline.com"}/api/documents.search`, {
   method: "POST",
   headers: {
     Authorization: `Bearer ${process.env.apiToken}`,
@@ -15,7 +15,7 @@ const results = await alfy.fetch(`${process.env.domain || "www.getoutline.com"}/
 let items = results.data.map(result => ({
   title: result.document.title,
   subtitle: result.context.replace(/\<\/?b\>/g, ''),
-  arg: `https://${process.env.domain || "www.getoutline.com"}${result.document.url}`,
+  arg: `https://${process.env.domain || "app.getoutline.com"}${result.document.url}`,
   icon: {
     path: path.join(__dirname, "document.png")
   }
@@ -24,7 +24,7 @@ let items = results.data.map(result => ({
 if (!items.length) {
   items = [{
     title: "No results - go to Outline homepage",
-    arg: `https://${process.env.domain || "www.getoutline.com"}`
+    arg: `https://${process.env.domain || "app.getoutline.com"}`
   }];
 }
 
